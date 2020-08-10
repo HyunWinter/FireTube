80fbbc72f083777d6e19d385dc9954ec246c6ed5
Jihyuk Chung <jihyuk.chung@oit.edu>
2020-08-09 21:43:57 -0700

    Temporary Fullscreen Youtube Player

--- app/src/main/java/com/hyun/firetube/activity/VideoPlayerActivity.kt
+++ app/src/main/java/com/hyun/firetube/activity/VideoPlayerActivity.kt
@@ -2,6 +2,8 @@ package com.hyun.firetube.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayer.OnInitializedListener
 import com.hyun.firetube.R
 import com.hyun.firetube.model.Video
 import kotlinx.android.synthetic.main.activity_videoplayer.*
@@ -32,10 +34,10 @@ class VideoPlayerActivity : BaseActivity() {
         val videoTitle = intent.getStringExtra(getString(R.string.Video_Title_Key)) as String
         this.mVideo = Video(videoID, videoTitle, "")
 
-        setSupportActionBar(this.VideoPlayer_Toolbar)
+        /*setSupportActionBar(this.VideoPlayer_Toolbar)
         supportActionBar?.title = ""
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)*/
     }
 
     /************************************************************************
@@ -46,6 +48,6 @@ class VideoPlayerActivity : BaseActivity() {
     private fun setContents() {
 
         this.VideoPlayer_Youtube.play(this.mVideo.id)
-        this.VideoPlayer_Title.text = this.mVideo.title
+        //this.VideoPlayer_Title.text = this.mVideo.title
     }
 }
